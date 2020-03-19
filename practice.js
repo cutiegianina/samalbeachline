27b57ddeb679ea79b76b3b66eba2f1f722064f0c
cutiegianina <principebragi12@gmail.com>
2020-03-19 18:04:40 +0800

    first commit

---
+++ practice.js
@@ -0,0 +1,52 @@
+var modal = document.getElementById('galleryy');
+var modal02 = document.getElementById('galleryy02');
+var modal03 = document.getElementById('galleryy03');
+var modal04 = document.getElementById('galleryy04');
+var modal05 = document.getElementById('galleryy05');
+var modal06 = document.getElementById('galleryy06');
+var modal07 = document.getElementById('galleryy07');
+var modal08 = document.getElementById('galleryy08');
+var modal08 = document.getElementById('galleryy09');
+
+function openModal(){
+	modal.style.display = 'block';
+}
+
+function openModal02(){
+	modal02.style.display = 'block';
+}
+
+function openModal03(){
+	modal03.style.display = 'block';
+}
+
+function openModal04(){
+	modal04.style.display = 'block';
+}
+
+function openModal05(){
+	modal05.style.display = 'block';
+}
+
+function openModal06(){
+	modal06.style.display = 'block';
+}
+
+function openModal07(){
+	modal07.style.display = 'block';
+}
+
+function openModal08(){
+	modal08.style.display = 'block';	
+}
+
+function openModal09(){
+	modal09.style.display = 'block';
+}
+
+
+
+
+var modalBtn = document.getElementById('beachfront04');	
+
+modalBtn.addEventListener('click', openModal);
